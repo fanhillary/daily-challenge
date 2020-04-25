cf1200bf5c2dd3d0c616ce26861da699fb63432b
Hillary Fan <hillary.fan123@gmail.com>
2020-04-24 18:14:30 -0700

    Updates

---
+++ precache-manifest.660ff4f1d3a49f5fcb33249db5501f75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eff1bf42f56f441eae431a2e75e49ab",
+    "url": "/daily-challenge/index.html"
+  },
+  {
+    "revision": "c1c6c8a8efaafb6af0cd",
+    "url": "/daily-challenge/static/css/main.c8c0f208.chunk.css"
+  },
+  {
+    "revision": "82655f42db3663d1e268",
+    "url": "/daily-challenge/static/js/2.3cbe4fac.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/daily-challenge/static/js/2.3cbe4fac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1c6c8a8efaafb6af0cd",
+    "url": "/daily-challenge/static/js/main.2a99be93.chunk.js"
+  },
+  {
+    "revision": "4996e1d244f792290592",
+    "url": "/daily-challenge/static/js/runtime-main.58c581d5.js"
+  }
+]);
