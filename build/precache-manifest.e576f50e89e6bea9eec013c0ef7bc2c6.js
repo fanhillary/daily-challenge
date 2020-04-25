1aacb27b5eecfe1f083aaf3bf64af825869e508a
Hillary Fan <hillary.fan123@gmail.com>
2020-04-24 18:25:07 -0700

    added basename of daily-challenge to router

---
+++ build/precache-manifest.e576f50e89e6bea9eec013c0ef7bc2c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8a73a1106803e5a93b76dc75927b47b",
+    "url": "/daily-challenge/index.html"
+  },
+  {
+    "revision": "ad3c7f6ec7b380a0dbfe",
+    "url": "/daily-challenge/static/css/main.c8c0f208.chunk.css"
+  },
+  {
+    "revision": "82655f42db3663d1e268",
+    "url": "/daily-challenge/static/js/2.3cbe4fac.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/daily-challenge/static/js/2.3cbe4fac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad3c7f6ec7b380a0dbfe",
+    "url": "/daily-challenge/static/js/main.8dd41ba0.chunk.js"
+  },
+  {
+    "revision": "4996e1d244f792290592",
+    "url": "/daily-challenge/static/js/runtime-main.58c581d5.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import {BrowserRouter as Router} from 'react-router-dom';
-ReactDOM.render(<Router><App /></Router>, document.getElementById("root"));
+ReactDOM.render(<Router basename="/daily-challenge"><App /></Router>, document.getElementById("root"));
 registerServiceWorker();
